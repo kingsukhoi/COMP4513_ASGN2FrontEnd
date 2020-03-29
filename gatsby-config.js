bc96cd827ccd15ea3c9dfa77075b6f45f9a77bfc
A-Arndt <aarnd649@mtroyal.ca>
2020-03-29 00:32:33 -0600

    install gatsby-plugin-create-client-paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,10 @@
 
 module.exports = {
   /* Your site config here */
-}
+  plugins:[
+      {
+      resolve: `gatsby-plugin-create-client-paths`,
+      options: { prefixes: [`/app/*`] },
+    },
+  ]
+};
---
+++ src/components/Login.js
@@ -0,0 +1,86 @@
+import React, {useState} from "react";
+import { Form, Input, Button, Alert } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+import '../style/Login.css'
+import { navigate } from "gatsby";
+import {handleLogin, isLoggedIn} from '../services/auth'
+// import { Button } from 'react-bulma-components/dist';
+
+
+
+const Login = () => {
+  console.log(isLoggedIn());
+  if(isLoggedIn()) {
+    navigate("/");
+  }
+  const [validLogin, setValidLogin] = useState(true);
+  const invalidLogin  = validLogin ? "" : <Alert 
+                                        message="Invalid username or password."
+                                        type="error"
+                                        banner
+                                        className="invalid-login"
+                                        />;
+
+  //Send the data from the form to the login handler in auth services
+  const postData = (formValues) => {
+    handleLogin(formValues)
+      .then( (loginResult)=> {
+        setValidLogin(loginResult);
+      }).catch(()=>{
+        console.error('Something went very wrong!');
+      });
+  }
+
+  return (
+    <div id="login-container">
+     
+      <Form
+        name="login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={postData}
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true, 
+              message: 'Please input your username.', 
+            }
+          ]}
+        >
+          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true, 
+              message: 'Please input your password.',
+            }
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+
+        <Form.Item >
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+      {invalidLogin}
+      
+    </div>
+  )
+};
+
+export default () => (<Login/>);
