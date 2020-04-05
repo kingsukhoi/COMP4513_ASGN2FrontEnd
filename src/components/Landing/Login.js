23e5cd5ed1d27649e353756b7185310bdaca89de
Trevlore <tbrow189@mtroyal.ca>
2020-04-05 09:54:41 -0600

    Routes to "Home.js" change to "Movies.js".

--- src/components/Landing/Login.js
+++ src/components/Landing/Login.js
@@ -10,7 +10,7 @@ import { handleLogin, isLoggedIn } from '../../services/auth'
 const Login = () => {
   console.log(isLoggedIn());
   if (isLoggedIn()) {
-    navigate("/app/");
+    navigate("/app/movies");
   }
   const [validLogin, setValidLogin] = useState(true);
   const invalidLogin = validLogin ? "" : <Alert
